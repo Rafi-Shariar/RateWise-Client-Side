9d6e61953d6f3e531924ad715a30cbba828cb7f2
Rafi-Shariar <rafi.shariar619@gmail.com>
2025-06-04 16:24:49 +0600

    banner section done with slider

--- src/Components/Banner/BannerSection.jsx
+++ src/Components/Banner/BannerSection.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 import Banner1 from './Banner1';
 import Banner2 from './Banner2';
 import Banner3 from './Banner3';
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const BannerSection = () => {
+    const settings = {
+    dots: true,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000, // 4 seconds
+    pauseOnHover: true,
+  };
     return (
-        <div>
-            <Banner1></Banner1>
-            <Banner2></Banner2>
-            <Banner3></Banner3>
+        <div className='overflow-hidden'>
+            <Slider {...settings}>
+                <div><Banner1/></div>
+                <div><Banner2/></div>
+                <div><Banner3/></div>
+            
+            </Slider>
         </div>
     );
 };
