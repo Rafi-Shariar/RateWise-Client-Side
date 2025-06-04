37f297cb12e56ceec4dbd8d7600b076e4d4bdf1c
Rafi-Shariar <rafi.shariar619@gmail.com>
2025-06-05 00:52:55 +0600

    removed featured section

--- src/Components/Shared/Loading..jsx
+++ src/Components/Shared/Loading..jsx
@@ -4,10 +4,10 @@ const Loading = () => {
   return (
     <div className="">
       <div className="flex w-52 flex-col gap-4">
-        <div className="skeleton h-32 w-full"></div>
-        <div className="skeleton h-4 w-28"></div>
-        <div className="skeleton h-4 w-full"></div>
-        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton bg-slate-200 h-32 w-full"></div>
+        <div className="skeleton bg-slate-200 h-4 w-28"></div>
+        <div className="skeleton bg-slate-200 h-4 w-full"></div>
+        <div className="skeleton bg-slate-200 h-4 w-full"></div>
       </div>
     </div>
   );
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import BannerSection from '../Components/Banner/BannerSection';
-import FeaturedContainer from '../Components/Featured/FeaturedContainer';
 
 const HomePage = () => {
 
@@ -8,7 +7,6 @@ const HomePage = () => {
     return (
         <div className='p-2'>
             <BannerSection></BannerSection>
-            <FeaturedContainer></FeaturedContainer>
         </div>
     );
 };
