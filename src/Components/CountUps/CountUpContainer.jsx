a0cb87a589002a2ce40f4c4278e6120944a07768
Rafi-Shariar <rafi.shariar619@gmail.com>
2025-06-07 11:46:51 +0600

    CountUp Section done

--- src/Components/CountUps/CountUpContainer.jsx
+++ src/Components/CountUps/CountUpContainer.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import serviceImage from "../../assets/CountUpImages/public-service.png";
-import UsersImage from "../../assets/CountUpImages/public-service.png";
+import UsersImage from "../../assets/CountUpImages/man.png";
 import RatingImage from "../../assets/CountUpImages/rating.png";
 import CountUp from "react-countup";
+import { motion } from "motion/react"
+
 const CountUpContainer = () => {
   const [data, setData] = useState(null);
 
@@ -13,11 +15,28 @@ const CountUpContainer = () => {
         setData(counts);
       });
   }, []);
+
+
+
+
+
   return (
-    <div className="my-10 flex flex-col md:flex-row justify-center gap-7">
+    <div>
+
+        <motion.div
+            initial={{ x: -400, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1, ease: "easeOut" }}>
+                <h1 className='text-2xl font-semibold text-blue-900 lg:text-4xl mt-20'>Operational Summary</h1>
+            <p className='font-extralight'>Tracking service activity, user engagement, and feedback.</p>
+            </motion.div>
+
+
+        {/* Matrices */}
+        <div className="my-10 flex flex-col md:flex-row justify-center gap-7 bg-slate-100 py-20 px-2 rounded-2xl">
 
         {/* Services */}
-      <div className="flex flex-col justify-center items-center bg-gradient-to-br from-white via-green-50 to-green-200 shadow-lg p-8 rounded-2xl border border-green-300">
+      <div className="flex flex-col justify-center items-center bg-gradient-to-br from-white via-green-50 to-green-200 shadow-lg p-8 rounded-2xl border border-green-300 min-w-[300px] shadow-gray-400">
         <img src={serviceImage} alt="Service Icon" className="w-16 mb-4" />
 
         <h1 className="text-4xl font-bold text-green-700">
@@ -35,7 +54,7 @@ const CountUpContainer = () => {
 
       {/* Users */}
 
-      <div className="flex flex-col justify-center items-center bg-gradient-to-br from-white via-orange-50 to-orange-200 shadow-lg p-8 rounded-2xl border border-orange-300 ">
+      <div className="flex flex-col justify-center items-center bg-gradient-to-br from-white via-orange-50 to-orange-200 shadow-lg p-8 rounded-2xl border border-orange-300 min-w-[300px] shadow-gray-400 ">
         <img src={UsersImage} alt="Service Icon" className="w-16 mb-4" />
 
         <h1 className="text-4xl font-bold text-orange-500">
@@ -53,7 +72,7 @@ const CountUpContainer = () => {
 
       {/* Reviews */}
 
-      <div className="flex flex-col justify-center items-center bg-gradient-to-br from-white via-yellow-50 to-yellow-100 shadow-lg p-8 rounded-2xl border border-yellow-300">
+      <div className="flex flex-col justify-center items-center bg-gradient-to-br from-white via-yellow-50 to-yellow-100 shadow-lg p-8 rounded-2xl border border-yellow-300 min-w-[300px] shadow-gray-400">
         <img src={RatingImage} alt="Service Icon" className="w-16 mb-4" />
 
         <h1 className="text-4xl font-bold text-yellow-600">
@@ -68,6 +87,7 @@ const CountUpContainer = () => {
             See what users are saying
         </p>
       </div>
+    </div>
     </div>
   );
 };
