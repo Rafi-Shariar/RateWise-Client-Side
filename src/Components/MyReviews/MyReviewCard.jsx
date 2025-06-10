c35a209b8dfa5837d32564da266093c75919d164
Rafi-Shariar <rafi.shariar619@gmail.com>
2025-06-10 15:29:44 +0600

    decorated edit review modal

--- src/Components/MyReviews/MyReviewCard.jsx
+++ src/Components/MyReviews/MyReviewCard.jsx
@@ -4,8 +4,11 @@ import { FiExternalLink } from "react-icons/fi";
 import { HiOutlinePencil } from "react-icons/hi2";
 import { MdDeleteOutline } from "react-icons/md";
 import Swal from "sweetalert2";
+import { RiHomeOfficeFill } from "react-icons/ri";
 import { Link } from "react-router";
-
+import { BiCategory } from "react-icons/bi";
+import { IoIosSave } from "react-icons/io";
+import { RxCross2 } from "react-icons/rx";
 const MyReviewCard = ({ review, onUpdated }) => {
   const modalRef = useRef();
   const [serviceData, setServiceData] = useState(null);
@@ -89,35 +92,56 @@ const MyReviewCard = ({ review, onUpdated }) => {
         >
           <HiOutlinePencil /> Edit
         </button>
-        <button className="btn btn-soft btn-error">
-          <MdDeleteOutline /> Delete
-        </button>
-      </div>
-
-      <dialog ref={modalRef} className="modal">
+        <dialog ref={modalRef} className="modal">
         <form method="dialog" onSubmit={handleSubmit} className="modal-box">
-          <h3 className="font-bold text-lg mb-4">Update Your Review</h3>
-          <Rating value={newRating} onChange={setNewRating} style={{ maxWidth: 120 }} />
+          <h3 className="font-bold text-lg text-cyan-500">Update Your Review</h3>
+            <div className="divider"></div>
+
+          <div>
+            <div>
+                <h1 className="text-2xl font-semibold text-gray-600">{serviceData.title}</h1>
+            </div>
+            <div className="my-3">
+                <h1 className="badge mr-3"><RiHomeOfficeFill />{serviceData.companyName}</h1>
+                <h1 className="badge"><BiCategory />{serviceData.category}</h1>
+            </div>
+          </div>
+          <div className="divider"></div>
+         <div className="flex items-center">
+            <h1 className="font-semibold text-yellow-500 text-xl">Rating:</h1>
+             <Rating value={newRating} onChange={setNewRating} style={{ maxWidth: 120 }} />
+         </div>
 
           <textarea
             name="description"
             value={description}
             onChange={e => setDescription(e.target.value)}
-            className="textarea w-full mt-4"
+            className="textarea w-full mt-4 text-gray-600"
             rows={4}
             required
           />
 
-          <div className="modal-action">
-            <button type="submit" className="btn btn-primary">
-              Save
+          <div className="modal-action flex justify-between">
+            <div>
+                 <h1 className="badge text-lg p-4">{review.addedDate}</h1>
+            </div>
+            <div className="flex gap-4">
+                <button type="submit" className="btn btn-success ">
+              <IoIosSave />Save
             </button>
-            <button type="button" className="btn" onClick={() => modalRef.current?.close()}>
-              Cancel
+            <button type="button" className="btn btn-error" onClick={() => modalRef.current?.close()}>
+              <RxCross2 />Cancel
             </button>
+            </div>
           </div>
         </form>
       </dialog>
+        <button className="btn btn-soft btn-error">
+          <MdDeleteOutline /> Delete
+        </button>
+      </div>
+
+      
     </div>
   );
 };
