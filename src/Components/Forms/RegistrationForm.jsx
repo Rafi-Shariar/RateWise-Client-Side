8b72938fbffc0199e274dd63097c0500e96a4589
Rafi-Shariar <rafi.shariar619@gmail.com>
2025-08-08 22:35:09 +0600

    register form updated

--- src/Components/Forms/RegistrationForm.jsx
+++ src/Components/Forms/RegistrationForm.jsx
@@ -67,7 +67,7 @@ const RegistrationForm = () => {
      initial={{ y: 150, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ type: "spring", stiffness: 50, damping: 15 }}>
-      <form action="" className="lg:w-2/3 bg-linear-to-r from-cyan-100 to-blue-100 p-7 rounded-2xl" 
+      <form action="" className="max-w-[500px] bg-linear-to-r from-cyan-100 to-blue-100 p-7 rounded-2xl" 
       onSubmit={handleRegistration}>
         <legend className="fieldset-legend">Name</legend>
         <input
