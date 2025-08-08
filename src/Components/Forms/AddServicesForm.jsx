0e70459916a8adb1c2d5f6ad331437aa6fc8058a
Rafi-Shariar <rafi.shariar619@gmail.com>
2025-08-08 22:28:03 +0600

    add service form updated

--- src/Components/Forms/AddServicesForm.jsx
+++ src/Components/Forms/AddServicesForm.jsx
@@ -4,7 +4,7 @@ import { toast, ToastContainer } from "react-toastify";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
 const AddServicesForm = ({ userInfo }) => {
   const notify = () => toast("New Service Added!");
-  const axiosSecure = useAxiosSecure()
+  const axiosSecure = useAxiosSecure();
 
   const handleAddService = (e) => {
     e.preventDefault();
@@ -18,114 +18,134 @@ const AddServicesForm = ({ userInfo }) => {
     const newServiceData = { ...serviceData, addedDate };
 
     //Adding data to DB
-    axiosSecure.post("/addservices", newServiceData)
-      .then(() => {
-        //sweetalert
-        notify();
-        e.target.reset();
-      });
+    axiosSecure.post("/addservices", newServiceData).then(() => {
+      //sweetalert
+      notify();
+      e.target.reset();
+    });
   };
   return (
     <div className="p-2 my-10">
       <form
         onSubmit={handleAddService}
         action=""
-        className=" bg-linear-to-r from-blue-900 to-sky-700 p-7 rounded-2xl lg:w-[500px]"
+        className=" bg-linear-to-r from-blue-900 to-sky-700 p-7 rounded-2xl w-[30px] lg:w-5xl grid grid-cols-1 lg:grid-cols-2 gap-10"
       >
-        <legend className="fieldset-legend text-white">Company Logo</legend>
-        <input
-          type="text"
-          className="input w-full"
-          placeholder="add company logo url..."
-          name="image"
-          required
-        />
+        <div>
+          <legend className="fieldset-legend text-white">Company Logo</legend>
+          <input
+            type="text"
+            className="input w-full"
+            placeholder="add company logo url..."
+            name="image"
+            required
+          />
+        </div>
 
-        <legend className="fieldset-legend text-white">Service title</legend>
-        <input
-          type="text"
-          className="input w-full"
-          placeholder="Enter title..."
-          name="title"
-          required
-        />
+        <div>
+          <legend className="fieldset-legend text-white">Service title</legend>
+          <input
+            type="text"
+            className="input w-full"
+            placeholder="Enter title..."
+            name="title"
+            required
+          />
+        </div>
 
-        <legend className="fieldset-legend text-white">Company Name</legend>
-        <input
-          type="text"
-          className="input w-full"
-          placeholder="Enter your photourl..."
-          name="companyName"
-          required
-        />
+        <div>
+          <legend className="fieldset-legend text-white">Company Name</legend>
+          <input
+            type="text"
+            className="input w-full"
+            placeholder="Enter your photourl..."
+            name="companyName"
+            required
+          />
+        </div>
 
-        <legend className="fieldset-legend text-white">Website</legend>
-        <input
-          type="text"
-          className="input w-full"
-          placeholder="Enter your website..."
-          name="website"
-          required
-        />
+        <div>
+          <legend className="fieldset-legend text-white">Website</legend>
+          <input
+            type="text"
+            className="input w-full"
+            placeholder="Enter your website..."
+            name="website"
+            required
+          />
+        </div>
 
-        <legend className="fieldset-legend text-white">Description</legend>
-        <textarea
-          type="text"
-          className="textarea w-full"
-          placeholder="Add description..."
-          name="description"
-          required
-        />
+        <div className="lg:col-span-2">
+          <legend className="fieldset-legend text-white">Description</legend>
+          <textarea
+            type="text"
+            className="textarea w-full h-[180px]"
+            placeholder="Add description..."
+            name="description"
+            required
+          />
+        </div>
 
-        <legend className="fieldset-legend text-white">Category</legend>
-        <select name="category" required className="input w-full">
-          <option value="">Select a category</option>
-          <option value="Web Development">Web Development</option>
-          <option value="Architecture & Interior">
-            Architecture & Interior
-          </option>
-          <option value="Electronics & Repair">Electronics & Repair</option>
-          <option value="Automobile Services">Automobile Services</option>
-          <option value="Retail & Stores">Retail & Stores</option>
-          <option value="Grocery & Super Shops">Grocery & Super Shops</option>
-          <option value="Graphic & Logo Design">Graphic & Logo Design</option>
-          <option value="Content & Copywriting">Content & Copywriting</option>
-          <option value="Video Production">Video Production</option>
-          <option value="Mobile App Development">Mobile App Development</option>
-          <option value="SEO & Marketing">SEO & Marketing</option>
-          <option value="UI/UX & Product Design">UI/UX & Product Design</option>
-          <option value="Social Media Services">Social Media Services</option>
-          <option value="Consulting & Strategy">Consulting & Strategy</option>
-          <option value="Home & Cleaning Services">
-            Home & Cleaning Services
-          </option>
-          
-        </select>
+        <div>
+          <legend className="fieldset-legend text-white">Category</legend>
+          <select name="category" required className="input w-full">
+            <option value="">Select a category</option>
+            <option value="Web Development">Web Development</option>
+            <option value="Architecture & Interior">
+              Architecture & Interior
+            </option>
+            <option value="Electronics & Repair">Electronics & Repair</option>
+            <option value="Automobile Services">Automobile Services</option>
+            <option value="Retail & Stores">Retail & Stores</option>
+            <option value="Grocery & Super Shops">Grocery & Super Shops</option>
+            <option value="Graphic & Logo Design">Graphic & Logo Design</option>
+            <option value="Content & Copywriting">Content & Copywriting</option>
+            <option value="Video Production">Video Production</option>
+            <option value="Mobile App Development">
+              Mobile App Development
+            </option>
+            <option value="SEO & Marketing">SEO & Marketing</option>
+            <option value="UI/UX & Product Design">
+              UI/UX & Product Design
+            </option>
+            <option value="Social Media Services">Social Media Services</option>
+            <option value="Consulting & Strategy">Consulting & Strategy</option>
+            <option value="Home & Cleaning Services">
+              Home & Cleaning Services
+            </option>
+          </select>
+        </div>
 
-        <legend className="fieldset-legend text-white">Price</legend>
-        <input
-          type="number"
-          className="input w-full"
-          placeholder="Price of your service..."
-          name="price"
-          required
-        />
+        <div>
+          <legend className="fieldset-legend text-white">Price</legend>
+          <input
+            type="number"
+            className="input w-full"
+            placeholder="Price of your service..."
+            name="price"
+            required
+          />
+        </div>
 
-        <legend className="fieldset-legend text-white">Added By</legend>
-        <input
-          type="text"
-          className="input w-full text-slate-400"
-          name="userEmail"
-          defaultValue={userInfo?.email}
-          readOnly
-        />
+        <div className="lg:col-span-2">
+          <legend className="fieldset-legend text-white">Added By</legend>
+          <input
+            type="text"
+            className=" text-slate-300"
+            name="userEmail"
+            defaultValue={userInfo?.email}
+            readOnly
+          />
+        </div>
 
-        <button
-          type="submit"
-          className="btn bg-yellow-500 text-white w-full mt-4 hover:bg-white hover:text-yellow-700"
-        >
-          Add Service
-        </button>
+        <div className="lg:col-span-2 flex justify-center">
+          <button
+            type="submit"
+            className="btn bg-yellow-500 text-blue-700 w-[150px] hover:bg-white hover:text-yellow-700"
+          >
+            Add Service
+          </button>
+        </div>
       </form>
       <ToastContainer />
     </div>
